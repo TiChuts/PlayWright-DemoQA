3e7bc050aa67526c4c9681e537f2a97c06b033a2
TiChuts <gavemesomething@gmail.com>
2026-03-03 23:01:35 +0700

    hahahaha

--- service/book-service.ts
+++ service/book-service.ts
@@ -2,6 +2,7 @@ import { API_DEMO_QA_BOOKS_ENDPOINTS } from "../constant/endpoints";
 import { APIClient } from "../core/api/api-client";
 import { LOGIN_DATA } from "../test-data/login-data";
 import { BOOK_DATA } from "../test-data/book-data";
+import { LoginService } from "../service/login-service";
 
 export class BookService {
   _client: APIClient;
@@ -10,9 +11,10 @@ export class BookService {
     this._client = apiClient;
   }
 
-  async addBooks(token: string) {
+  async addBooks(isbn: string) {
+    const loginService = new LoginService(this._client);
+    const token = await loginService.getAccessToken();
     const account = LOGIN_DATA["test_account_01"];
-    const book_isbn = BOOK_DATA["valid_book_1"];
 
     return await this._client.post(API_DEMO_QA_BOOKS_ENDPOINTS.ADD_BOOKS, {
       headers: {
@@ -23,7 +25,7 @@ export class BookService {
         userId: account.userId,
         collectionOfIsbns: [
           {
-            isbn: book_isbn.isbn,
+            isbn: isbn,
           },
         ],
       },
